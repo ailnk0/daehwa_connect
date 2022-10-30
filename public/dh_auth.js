0aac4fccbf6dcb4800077fb543748b97c165c8fd
Bundo Lee <ailnk@naver.com>
2022-10-30 18:14:26 +0900

    #5 사용자는 사진이 포함된 글을 남길 수 있다
    [개요]
    - 사용자는 사진이 포함된 게시글을 올릴 수 있다
    - 게시글은 메인 페이지에 캐로샐로 노출된다
    - 게시글에는 작성자 이름, 올린 날짜, 내용이 포함된다
    
    [수정]
    - 제목, 내용, 사진이 포함된 게시글을 올릴 수 있는 기능 추가
    - 게시글은 메인 페이지에 리스트 형태로 노출
    - 게시글 편집 기능 추가
    - 게시글 삭제 기능 추가 (리소스 제거 포함)

--- public/dh_auth.js
+++ public/dh_auth.js
@@ -1,12 +1,102 @@
 class Firebase {
-    static app;
-    static db;
-    static auth;
-    static storage;
+    static app = null;
+    static db = null;
+    static auth = null;
+    static storage = null;
+    static currentUser = {
+        auth_data: null,
+        custom_data: null,
+    };
 }
 
 function errorMessage(error) {
     alert(error);
+}
+
+function initIndex() {
+    if ($("#post-carousel").length <= 0) {
+        return;
+    }
+
+    Firebase.db.collection('write-post').get().then((result) => {
+        result.forEach((doc) => {
+
+            let template = `
+                <div class="post">
+                    <div class="post-header">
+                        <div class="profile"></div>
+                        <span class="profile-name">${doc.data().name}</span>
+                        <button class="btn btn-light" style="float:right;" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <i class="bi-three-dots-vertical"></i>
+                        </button>
+                        <ul class="dropdown-menu dropdown-menu-end">
+                            <li><a class="dropdown-item" href="edit.html?id=${doc.id}">편집</a></li>
+                            <li><a class="dropdown-item" href="delete.html?id=${doc.id}">삭제</a></li>
+                        </ul>
+                    </div>
+                    <div class="post-body" style="background-image: url('${doc.data().image}');"></div>
+                    <div class="post-content">
+                        <p>43 Likes</p>
+                        <p><strong>${doc.data().name}</strong> 너무 귀여워요!</p>
+                        <p class="date">${doc.data().date.toDate().toLocaleString()}</p>
+                    </div>
+                </div>
+                `;
+
+            $('#post-carousel').append(template);
+        });
+
+
+    });
+}
+
+function initEdit() {
+    if ($("#edit-title").length <= 0) {
+        return;
+    }
+
+    let queryString = new URLSearchParams(window.location.search);
+    let postId = queryString.get('id');
+
+    Firebase.db.collection('write-post').doc(postId).get().then((result) => {
+        detail = result.data();
+
+        $('#edit-title').val(result.data().title);
+        $('#edit-content').html(result.data().content);
+    });
+}
+
+function initDelete() {
+    if ($("#delete-post").length <= 0) {
+        return;
+    }
+
+    let queryString = new URLSearchParams(window.location.search);
+    let postId = queryString.get('id');
+
+    Firebase.db.collection('write-post').doc(postId).get()
+        .then((result) => {
+            if (result.data().imagePath) {
+                const storageRef = Firebase.storage.ref();
+                var desertRef = storageRef.child(result.data().imagePath);
+                desertRef.delete()
+                    .then(() => {
+                    })
+                    .catch((error) => {
+                        assert(error);
+                    });
+            }
+
+            Firebase.db.collection("write-post").doc(postId).delete()
+                .then(() => {
+                    alert("게시물이 삭제되었습니다!");
+                    window.location.href = 'index.html';
+                }).catch((error) => {
+                    alert("게시물을 삭제할 수 없습니다!\n" + error);
+                    window.location.href = 'index.html';
+                });
+        });
+
 }
 
 function registerEvent() {
@@ -23,6 +113,14 @@ function registerEvent() {
         window.location.href = "index.html";
     });
 
+    $('#write-post').click(function () {
+        writePost();
+    });
+
+    $('#edit-post').click(function () {
+        editPost();
+    });
+
     Firebase.auth.onAuthStateChanged(function (user) {
         if (user) {
             Firebase.db
@@ -30,6 +128,9 @@ function registerEvent() {
                 .doc(user.uid)
                 .get()
                 .then((result) => {
+                    Firebase.currentUser.auth_data = user;
+                    Firebase.currentUser.custom_data = result.data();
+
                     $('#userName').html(result.data().name);
                     $('#userEmail').html(user.email);
 
@@ -37,6 +138,9 @@ function registerEvent() {
                     $('#usermenu-signedin').removeClass('d-none');
                 });
         } else {
+            Firebase.currentUser.auth_data = null;
+            Firebase.currentUser.custom_data = null;
+
             $('#userName').html('');
             $('#userEmail').html('');
 
@@ -55,7 +159,7 @@ function signIn() {
             window.location.href = "index.html";
         })
         .catch(function (error) {
-            errorMessage(error);
+            alert(error);
         });
 }
 
@@ -68,7 +172,7 @@ function signUp() {
             updateUserData(response.user);
         })
         .catch(function (error) {
-            errorMessage(error);
+            alert(error);
         });
 
 }
@@ -88,6 +192,63 @@ function updateUserData(user) {
         })
         .then(function (response) {
             window.location.href = "index.html";
+        });
+}
+
+function editPost() {
+    if (Firebase.currentUser.auth_data == null) {
+        return;
+    }
+
+    let queryString = new URLSearchParams(window.location.search);
+    let postId = queryString.get('id');
+
+    Firebase.db.collection('write-post').doc(postId).update({
+        title: $('#edit-title').val(),
+        content: $('#edit-content').val(),
+        date: new Date(),
+    }).then((result) => {
+        window.location.href = "index.html";
+    }).catch((err) => {
+        alert(err);
+    });
+}
+
+function writePost() {
+    if (Firebase.currentUser.auth_data == null) {
+        return;
+    }
+
+    let file = $('#write-image').prop('files')[0];
+    let fileId = Date.now().toString(36) + Math.random().toString(36).substr(2);
+
+    const storageRef = Firebase.storage.ref();
+    const uploadTask = storageRef.child('write-image/' + fileId).put(file);
+
+    uploadTask.on('state_changed',
+        // 변화시 동작하는 함수 
+        null,
+        // 에러시 동작하는 함수
+        (error) => {
+            alert(error);
+        },
+        // 성공시 동작하는 함수
+        () => {
+            uploadTask.snapshot.ref.getDownloadURL().then((url) => {
+                Firebase.db.collection('write-post').add({
+                    title: $('#write-title').val(),
+                    content: $('#write-content').val(),
+                    image: url,
+                    imagePath: 'write-image/' + fileId,
+                    date: new Date(),
+                    uid: Firebase.currentUser.auth_data.uid,
+                    name: Firebase.currentUser.custom_data.name,
+                }).then((result) => {
+                    window.location.href = "index.html";
+                }).catch((err) => {
+                    alert(err);
+                });
+            });
         });
 }
 
@@ -114,6 +275,10 @@ document.addEventListener('DOMContentLoaded', function () {
         Firebase.storage = firebase.storage();
 
         registerEvent();
+
+        initIndex();
+        initEdit();
+        initDelete();
 
     } catch (e) {
         console.error(e);
