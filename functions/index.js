72f6a56bd27da498104f71d15a345a61c5cb4ae4
Bundo Lee <ailnk@naver.com>
2022-11-10 18:40:14 +0900

    테스트용 function의 지역을 서울로 설정

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,10 @@
 const functions = require("firebase-functions");
+
+// The Firebase Admin SDK to access Firestore.
 const admin = require("firebase-admin");
 const serviceAccount = require(
     "daehwa-connect-firebase-adminsdk-9pqmd-4162e53b6a.json"
 );
-
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
@@ -11,10 +12,11 @@ admin.initializeApp({
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
+exports.helloWorld = functions.region("asia-northeast3")
+    .https.onRequest((request, response) => {
+      functions.logger.info("Hello logs!", {structuredData: true});
+      response.send("Hello from Firebase!");
+    });
 
 // exports.createAlert = functions.region('asia-northeast3')
 // .firestore.document('chatroom/{docid}').onCreate((snapshot, context) => {
